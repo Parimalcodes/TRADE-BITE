53ae72039bca2f95b564ae13c967d9273c17ce36
Hiral Patel <hiralpatel160302@gmail.com>
2025-11-09 13:08:05 +0530

    simplify structure

--- email-service/src/config/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-    emailUser: process.env.EMAIL_USER,
-    emailPass: process.env.EMAIL_PASS,
-    port: process.env.PORT || 3000,
-};
